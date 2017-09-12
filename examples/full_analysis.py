d76be43bf0845a0baadc750f86f85634c5099bd0
Thomas Malkin <Thomas.Malkin@dixon.com.au>
2017-09-12 17:24:02 +1000

    update example

--- examples/full_analysis.py
+++ examples/full_analysis.py
@@ -1,8 +1,8 @@
-import fyn
+from fyn import fyn
 import pandas as pd
 
 price_data = pd.read_csv("pricedata.csv", index_col=0)
 
-analysis = fyn.fyn(price_df=price_data)
+analysis = fyn(price_df=price_data)
 
 analysis.key_metrics()
