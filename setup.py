32d49bcb4c9be0ab9eccd3aa5b473b745a21405b
Thomas Malkin <Thomas.Malkin@dixon.com.au>
2017-09-12 16:47:09 +1000

    add setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='fyn',
+      version='0.0.1',
+      description='fynancial analysis',
+      author='Thomas Malkin',
+      author_email='tommalkin28@gmail.com',
+      packages=['fyn'],
+     )
