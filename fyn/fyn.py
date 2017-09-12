fe2e702074fb88b3198a30474161c77524c86294
Thomas Malkin <Thomas.Malkin@dixon.com.au>
2017-09-12 16:40:14 +1000

    add transform templates

--- fyn/fyn.py
+++ fyn/fyn.py
@@ -1,6 +1,15 @@
 import pandas as pd
+import numpy as np
+
 from .loader import load_price_data, load_income_data
 from .cleaner import clean_price_data, clean_income_data
+from .transformer import transform_price_data, transform_income_data
+
+
+class Analysis:
+    """ analysis of transformed data """
+
+
 
 def fyn(price_df, income_df=None):
     """
@@ -18,3 +27,9 @@ def fyn(price_df, income_df=None):
 
     if income_df:
         income_df = clean_income_data(income_df)
+
+    # transform dataframes
+    price_df = transform_price_data(price_df)
+
+    if income_df:
+        income_df = transform_income_data(income_df)
--- fyn/transformer.py
+++ fyn/transformer.py
@@ -1,1 +1,13 @@
-""" check dataframes
+""" transform dataframes to preferred format """
+
+def transform_price_data(price_df):
+    """transform price data"""
+
+    return price_df
+
+
+
+def transform_income_data(income_df):
+    """transform income data"""
+
+    return income_df
