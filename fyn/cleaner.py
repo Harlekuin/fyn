e030ef609d5e841ffbc803258b94530243ce698e
Tom Malkin <tommalkin28@gmail.com>
2017-09-16 19:05:17 +1000

    add check_weekends fundction in cleaner

--- fyn/cleaner.py
+++ fyn/cleaner.py
@@ -1,6 +1,8 @@
 """ check and clean dataframes """
 from .config import FILL_MISSING_PRICES
 import pandas as pd
+from datetime import date
+import calendar
 
 
 def clean_price_data(price_df):
@@ -29,3 +31,12 @@ def clean_income_data(income_df):
     """
 
     return income_df
+
+def check_weekends(date_list):
+    """ given a list of dates, check if any are weekends """
+
+    for my_date in date_list:
+        if calendar.day_name[my_date.weekday()] in ['Saturday', 'Sunday']:
+            return False
+
+    return True
