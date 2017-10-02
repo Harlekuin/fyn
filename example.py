a7b8a8c83f4c1fc4f6afd0b0ac5f5d7ef745c725
Tom Malkin <tommalkin28@gmail.com>
2017-10-02 18:34:07 +1100

    alter example.py to use os module

--- example.py
+++ example.py
@@ -1,7 +1,11 @@
 from fyn import fyn
 import pandas as pd
+import os
 
-price_data = pd.read_csv(".\examples\example_data.csv", index_col=0)
+data_file = os.path.join(os.getcwd(), "examples/example_data.csv")
+
+# price_data = pd.read_csv(".\examples\example_data.csv", index_col=0)
+price_data = pd.read_csv(data_file, index_col=0)
 
 analysis = fyn(price_df=price_data)
 
