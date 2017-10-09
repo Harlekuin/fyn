9ec93d027a869be539fd896d196bda16a763c41f
Thomas Malkin <Thomas.Malkin@evansdixon.com.au>
2017-10-09 22:01:58 +1000

    change income example data to excel format to fix date issues

--- example.py
+++ example.py
@@ -3,11 +3,11 @@ import pandas as pd
 import os
 
 price_data_file = os.path.join(os.getcwd(), "examples/example_data.csv")
-income_data_file = os.path.join(os.getcwd(), "examples/example_income.csv")
+income_data_file = os.path.join(os.getcwd(), "examples/example_income.xlsx")
 
 # price_data = pd.read_csv(".\examples\example_data.csv", index_col=0)
 price_data = pd.read_csv(price_data_file, index_col=0)
-income_data = pd.read_csv(income_data_file, index_col=0)
+income_data = pd.read_excel(income_data_file, index_col=0)
 
 analysis = fyn(price_df=price_data, income_df=income_data)
 
