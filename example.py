fe7e110edffdf3e139f00b05307624b898b3657b
Thomas Malkin <Thomas.Malkin@evansdixon.com.au>
2017-10-11 10:30:27 +1000

    add transaction examples and change example data to xlsx format

--- example.py
+++ example.py
@@ -2,10 +2,10 @@ from fyn import fyn
 import pandas as pd
 import os
 
-price_data_file = os.path.join(os.getcwd(), "examples/example_data.csv")
+price_data_file = os.path.join(os.getcwd(), "examples/example_data.xlsx")
 income_data_file = os.path.join(os.getcwd(), "examples/example_income.xlsx")
 
-price_data = pd.read_csv(price_data_file, index_col=0)
+price_data = pd.read_excel(price_data_file, index_col=0)
 income_data = pd.read_excel(income_data_file, index_col=0)
 
 analysis = fyn(price_df=price_data, income_df=income_data)
